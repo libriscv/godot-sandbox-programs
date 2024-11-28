c484222dd7af75b2032f60fc86e7a1ccb52800eb
Alf-André Walla <fwsgonzo@hotmail.com>
2024-11-28 18:40:06 +0100

    Simplify assembler program

--- programs/asm/main.cpp
+++ programs/asm/main.cpp
@@ -1,39 +1,21 @@
 #include <api.hpp>
-#include <libriscv/machine.hpp>
+#include <cstring>
 #include <sys/mman.h>
 using AsmCallback = Variant(*)();
-using namespace riscv;
-using machine_t = riscv::Machine<riscv::RISCV64>;
-
-EMBED_BINARY(asmjit, "asmjit/assembler");
-static const std::string_view asmjit_bin(asmjit, asmjit_size);
+struct AsmjitResult {
+	unsigned char *data;
+	size_t size;
+};
+extern "C" struct AsmjitResult assemble_to(const char *input, size_t size);
 
 static Variant assemble(String input) {
-	static machine_t master_machine(asmjit_bin, {
-		.memory_max = 4UL << 20,
-		.stack_size = 256UL << 10,
-		.use_memory_arena = false,
-	});
-	machine_t machine(master_machine, {
-		.memory_max = 4UL << 20,
-		.stack_size = 256UL << 10,
-		.use_memory_arena = false,
-	});
-	machine.setup_linux_syscalls(false, false);
-	machine.setup_linux({"asmjit", input.utf8(), "stuff"}, {"LC_ALL=C", "USER=guest"});
-
-	machine.cpu.simulate_inaccurate(machine.cpu.pc());
-
-	const uint64_t data = machine.cpu.reg(10);
-	const size_t   size = machine.cpu.reg(11);
-	if (data == 0) {
-		fprintf(stderr, "Failed to assemble program\n");
-		return Variant();
-	}
+	// Assemble the input
+	const std::string input_str = input.utf8();
+	AsmjitResult result = assemble_to(input_str.data(), input_str.size());
 
 	// After simulation, A0 contains assembled program, A1 size
-	std::string_view program = machine.memory.memview(data, size);
-	const char *program_data = program.data();
+	const uint8_t *program_data = result.data;
+	const size_t   size         = result.size;
 
 	void *executable = mmap(nullptr, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	std::memcpy(executable, program_data, size);
