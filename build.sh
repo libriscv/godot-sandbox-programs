d685215d097709981e92c9d9434509cbc2bbf3e5
Alf-André Walla <fwsgonzo@hotmail.com>
2024-12-06 17:12:32 +0100

    Build LuaJit for Zig toolchain too

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+export CC="riscv64-linux-gnu-gcc-12"
+export CXX="riscv64-linux-gnu-g++-12"
+
 mkdir -p .build
 pushd .build
 cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX .. -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON
