df427dac5695724f902b2d7096f1e38088c7ba4a
Alf-André Walla <fwsgonzo@hotmail.com>
2024-12-09 07:20:04 +0100

    Simplify zig.sh and CMakeLists.txt

--- zig.sh
+++ zig.sh
@@ -1,7 +1,8 @@
-TOOLCHAIN=$PWD/cmake/zig-toolchain.cmake
+export CC="zig;cc;-target riscv64-linux-musl"
+export CXX="zig;c++;-target riscv64-linux-musl"
 
 mkdir -p .zig
 pushd .zig
-cmake .. -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" -DEMBEDDED_RISCV=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX"
 make -j8
 popd
